0f04de108d88d1da0ba6232fa9d344c3579fe148
root <edas007@gmail.com>
2018-10-01 09:53:59 +0000

    segundo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      text: ''
+    }
+  }
+  textoCambiado = (algo) => {
+    this.setState(() => ({
+      text: algo
+    }))
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +21,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+    {JSON.stringify(this.state)}
         <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
+          <input
+			onChange={(evento) => this.textoCambiado(evento.target.value)}
+			type="text"
+			placeholder="Say Something" />
+          <p className="echo">Echo:{this.state.text}</p>
           <p>This should mirror the text you typed into the input field.</p>
         </div>
       </div>
